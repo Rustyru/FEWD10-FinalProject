c5eb21cd9b2894d56ecfa182731b0aad1083524b
Rustyru <rgirvine@Catherines-MacBook-Air.local>
2016-03-16 18:04:11 +1100

    banner shrink

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
-    $('.hamburger').click(function(){
-    //alert("click");
+    $('.hamburger').click(function () {
+        //alert("click");
         $(this).next().slideToggle();
+    });
+    $(window).scroll(function () {
+        var scrl = $(document).scrollTop();
+        if ($(document).scrollTop() > 10) {
+            $('.banner').addClass('shrink');
+        } else {
+            $('.banner').removeClass('shrink');
+        }
     });
 });
